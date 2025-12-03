74eeeceb7b25a8c0d0a0838e4729cdc75fd2f12e
Snappy <reasonableheir@gmail.com>
2025-12-03 14:45:31 -0700

    Initial commit

---
+++ src/main/java/sl/api/dai/DaiPIClient.java
@@ -0,0 +1,10 @@
+package sl.api.dai;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DaiPIClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
